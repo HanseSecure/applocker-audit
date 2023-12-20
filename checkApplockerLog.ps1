41133d5a283263024ff1ddd19e1708044dce220b
Florian Hansemann <info@hansesecure.de>
2023-12-20 08:35:10 +0100

    Update checkApplockerLog.ps1
    text file erweitern nicht überschreiben

--- checkApplockerLog.ps1
+++ checkApplockerLog.ps1
@@ -9,7 +9,7 @@ $system = $args[0]
 
 rm temp.txt
 echo "Is blocked by applocker"
-Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Applocker/*"; id=8004} -ComputerName $system | select Message -unique | Out-File temp.txt #-ComputerName WVDES230002 
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Applocker/*"; id=8004} -ComputerName $system | select Message -unique | Out-File -append temp.txt #-ComputerName WVDES230002 
 cat temp.txt | foreach {echo $_.split(" ")[3]} | out-file blocked-by-applocker.txt
 cat blocked-by-applocker.txt
 
@@ -17,7 +17,7 @@ sleep 5
 
 rm temp.txt
 echo "will be blocked by applocker"
-Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Applocker/*"; id=8003} -ComputerName $system| select Message -unique | Out-File temp.txt #-ComputerName WVDES230002 
+Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Applocker/*"; id=8003} -ComputerName $system| select Message -unique | Out-File -append temp.txt #-ComputerName WVDES230002 
 cat temp.txt | foreach {echo $_.split(" ")[3]} | out-file willbeblocked-by-applcoker.txt
 cat willbeblocked-by-applcoker.txt
 }
